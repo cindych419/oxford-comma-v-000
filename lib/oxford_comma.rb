fa7382a7dd13f5244e7f597cb5a09fcdebb5b415
cindych419@gmail.com <cindych419@gmail.com>
2018-05-01 21:13:26 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,7 +2,7 @@ def oxford_comma(array)
   if array.size ==2
     return "#{array[0]} and #{array[1]}"
   elsif 2< array.size
-  array.insert(-2, "and")
+  array[-1].insert(0, "and")
 end  
 array.join(", ")
 end
